f235e142fa674fd94e29e15b3c65945b39de2638
vkola <vkolachi@gmail.com>
2023-07-08 16:18:49 +0530

    new

--- stackAndQueue/stack.java
+++ stackAndQueue/stack.java
@@ -9,6 +9,15 @@ public class stack{
         System.out.println(st.add(111));
         System.out.println(st);
         System.out.println("first project");
+        //all the stack operations will take 0(1) time
+        System.out.println(st);
+        st.add(111);
+        System.out.println(st.peek());
+
+        //void do(Stack<Integer> st)   like this it will be defined
+        //anything that is initialized with new keyword tht will be stored in heap memory(dynamic memory)
+        //others (int ,string ) these will be stored in stack memory(static memory)
+         
         
     }
    
