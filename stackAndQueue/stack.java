b0415771d091f2bf83554714a5a44d128772072c
vkola <vkolachi@gmail.com>
2023-07-08 15:36:07 +0530

    first commit

---
+++ stackAndQueue/stack.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class stack{
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.add(1);
+        st.add(22);
+        System.out.println(st.pop());
+        System.out.println(st.add(111));
+        System.out.println(st);
+        System.out.println("first project");
+        
+    }
+   
+}
